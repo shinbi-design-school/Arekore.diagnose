9196616ff12d81f69394ddabf1049d67a61fff95
user <user@DESKTOP-R5JLT34>
2021-10-22 15:47:33 +0900

    bts診断

---
+++ src/main/java/cc/shinbi/bts/Const.java
@@ -0,0 +1,6 @@
+package cc.shinbi.bts;
+
+public class Const {
+	public static final String APP_NAME = "bts";
+
+}
---
+++ src/test/java/cc/shinbi/bts/test/BasicTest.java
@@ -0,0 +1,14 @@
+package cc.shinbi.bts.test;
+
+import org.junit.Test;
+
+import cc.shinbi.bts.Const;
+
+public class BasicTest {
+
+	@Test
+	public void test() {
+		System.out.println(Const.APP_NAME);
+	}
+
+}
