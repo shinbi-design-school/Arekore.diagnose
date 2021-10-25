5bd869388647e7e3961f4d4b35d319a38f81333f
das <das@MyComputer>
2021-10-25 11:54:46 +0900

    bts

--- src/main/java/cc/shinbi/bts/Const.java
+++ src/main/java/cc/shinbi/bts/Const.java
@@ -1,10 +1,7 @@
 package cc.shinbi.bts;
 
 public class Const {
-<<<<<<< HEAD
-	public static final String APP_NAME = "bts";
 
-=======
 	public static final String APP_NAME = "(bts)";
->>>>>>> branch 'master' of git@github.com:shinbi-design-school/Arekore.diagnose.git
+	
 }
--- src/test/java/cc/shinbi/bts/test/BasicTest.java
+++ src/test/java/cc/shinbi/bts/test/BasicTest.java
@@ -1,6 +1,6 @@
 package cc.shinbi.bts.test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import cc.shinbi.bts.Const;
 
